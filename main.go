b92be4125be8d574c8f2462e846afe41b8cabda1
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-10-11 11:45:24 +0700

    feat: added port env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,8 +13,13 @@ func main() {
 		fmt.Fprintf(w, "hello world\n")
 	})
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
 	fmt.Println("Server is running at http://localhost:8080")
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(fmt.Sprint(":", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
