d30e69cd1cf8e6ff5d10ce42bdbd8a9142245c02
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-10-30 12:45:48 +0700

    feat: move db to handler

--- main.go
+++ main.go
@@ -12,24 +12,32 @@ import (
 
 func main() {
 	// test
-	poolConfig, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatalln("Unable to parse DATABASE_URL:", err)
-	}
-
-	db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
-	if err != nil {
-		log.Fatalln("Unable to create connection pool:", err)
-	}
-
-	defer db.Close()
-
-	if err := db.QueryRow(context.Background(), "SELECT 1").Scan(new(int)); err != nil {
-		log.Fatalln("Unable to connect to database:", err)
-	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		log.Printf("Request received from %s\n", req.RemoteAddr)
+
+		poolConfig, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+		if err != nil {
+			log.Fatalln("Unable to parse DATABASE_URL:", err)
+			fmt.Fprintf(w, "Unable to parse DATABASE_URL: %s\n", err)
+			return
+		}
+
+		db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+		if err != nil {
+			log.Fatalln("Unable to create connection pool:", err)
+			fmt.Fprintf(w, "Unable to create connection pool: %s\n", err)
+			return
+		}
+
+		defer db.Close()
+
+		if err := db.QueryRow(context.Background(), "SELECT 1").Scan(new(int)); err != nil {
+			log.Fatalln("Unable to connect to database:", err)
+			fmt.Fprintf(w, "Unable to connect to database: %s\n", err)
+			return
+		}
+
 		fmt.Fprintf(w, "hello world!!! brilan\n")
 	})
 
@@ -40,7 +48,7 @@ func main() {
 
 	log.Printf("Server is running at :%s\n", port)
 
-	err = http.ListenAndServe(fmt.Sprint(":", port), nil)
+	err := http.ListenAndServe(fmt.Sprint(":", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
