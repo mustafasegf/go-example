67bfd390217942b216a82580cd7ff7643f9f8d00
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-12-08 21:44:20 +0700

    remove db

--- main.go
+++ main.go
@@ -1,45 +1,22 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	// test
-	poolConfig, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Println("Unable to parse DATABASE_URL:", err)
-		return
-	}
 
-	db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
-	if err != nil {
-		log.Println("Unable to create connection pool:", err)
-		return
-	}
-
-	defer db.Close()
-
-	_, err = db.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS test (id serial primary key, name text not null);")
-	if err != nil {
-		log.Println("Unable to create table:", err)
-		return
+	// print all envs
+	for _, e := range os.Environ() {
+		fmt.Println(e)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		log.Printf("Request received from %s\n", req.RemoteAddr)
-
-		if err := db.QueryRow(context.Background(), "SELECT 1").Scan(new(int)); err != nil {
-			log.Println("Unable to connect to database:", err)
-			fmt.Fprintf(w, "Unable to connect to database: %s\n", err)
-			return
-		}
 
 		fmt.Fprintf(w, "hello world!!!\n")
 	})
@@ -59,7 +36,7 @@ func main() {
 
 	log.Printf("Server is running at :%s\n", port)
 
-	err = http.ListenAndServe(fmt.Sprint(":", port), nil)
+	err := http.ListenAndServe(fmt.Sprint(":", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
