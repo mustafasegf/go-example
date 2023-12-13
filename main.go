706b85c6b6822cf57954275b1aec9874374a78f4
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-12-13 22:02:00 +0700

    feat: db

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
@@ -14,6 +17,16 @@ func main() {
 	for _, e := range os.Environ() {
 		fmt.Println(e)
 	}
+
+	poolConfig, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+
+	db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+	if err != nil {
+		log.Println("Unable to create connection pool:", err)
+		return
+	}
+
+	defer db.Close()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		log.Printf("Request received from %s\n", req.RemoteAddr)
